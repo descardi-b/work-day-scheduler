7286f90340378020a6e7eade0389f675c912547b
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-01-22 14:24:40 -0500

    fixing css attributes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,39 @@
-$("[id=task-box]").click(function() {
-    var inputBox = "<input>";
-    $(inputBox).addClass("form-control");
-    $(this).replaceWith(inputBox);
 
-    $("[id=task-box]").change(function() {
-    $(inputBox).replaceWith($(this));
-    });
+// edit task option event listener
+$(".container").on("click", "div.task-box", function () {
+
+    console.log($(this));
+
+    var taskBoxClass = $(this).attr("class");
+
+    var text = $(this)
+        .text()
+        .trim();
+    console.log(text);
+
+    var textInput = $("<textarea>")
+        .addClass(`form-control ${taskBoxClass}`)
+        .val(text);
+
+    $(this).after(textInput).remove();
+
+    textInput.trigger("focus");
 });
 
-var saveTask = function() {
-    $(".saveBtn").click(function() {
-    var inputText = inputBox.text();
-    localStorage.setItem(thisTask, inputText);
-})
-}
+$(".container").on("blur", "textarea", function () {
+    // get the textarea's current value/text
+    var text = $(this)
+        .val()
+        .trim();
 
+    var taskBoxClass = $(this).attr("class");
+
+    var taskP = $("<div>")
+        .addClass(taskBoxClass)
+        .removeClass("form-control")
+        .html(text);
+
+    $(this).after(taskP).remove();
+});
+
+ //  $(".container")
